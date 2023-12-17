de7d40ee273a911924f4bd3f7f0317e8dc15bec0
Fortino Romero <2311080254@alumno.utpuebla.edu.mx>
2023-12-17 17:26:36 -0600

    Modificando la lógica del juego

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -33,26 +33,27 @@ if (message.Contains("fox")){
 
 Random dice = new Random(); 
 int rollOne = dice.Next(1,7);
-int rollTwo = dice.Next(1,7);  
-int rollThree = dice.Next(1,7); 
+int rollTwo = dice.Next(1,7);
+int rollThree = dice.Next(1,7);
 int total = rollOne + rollTwo + rollThree; 
 
 Console.WriteLine($"Dice roll: {rollOne}, {rollTwo}, {rollThree} = {total}"); 
 
 if (rollOne == rollTwo || rollOne == rollThree || rollTwo == rollThree){
-    Console.WriteLine("You rolled doubles!! +2 bonus to total");
-    total += 2;  
-}
-
-if (rollOne == rollTwo && rollTwo == rollThree){
+    if (rollOne == rollTwo && rollTwo == rollThree){
     Console.WriteLine("You rolled triples!! +6 bonus to total");
-    total += 2;  
+    total += 6; 
+    Console.WriteLine(total);  
+} else {
+    Console.WriteLine("You rolled doubles!! +2 bonus to total");
+    total += 2;
+    Console.WriteLine(total);   
+}
 }
 
 if (total >= 15){
     Console.WriteLine("You win!!");
-}
-if (total < 15){
+} else {
     Console.WriteLine("Sorry, you lose."); 
 }
 
