7458e52dbfaa03b8e739710e69fca32626b0bc0c
Fortino Romero <2311080254@alumno.utpuebla.edu.mx>
2023-12-17 14:01:45 -0600

    Utilizando la documentación de Microsoft para investigar métodos

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,10 +1,18 @@
 ﻿// Ejercicio de dados
- 
-Random dice = new Random(); 
+
+/*Random dice = new Random(); 
 int rollOne = dice.Next();
 int rollTwo = dice.Next(101); 
 int rollThree = dice.Next(50, 101);
 
 Console.WriteLine($"First roll: {rollOne}");
 Console.WriteLine($"Second roll: {rollTwo}"); 
-Console.WriteLine($"Thrird roll: {rollThree}");
+Console.WriteLine($"Thrird roll: {rollThree}");*/
+
+//El mayor de dos numeros
+
+
+int fisrtValue = 500; 
+int secondValue = 600; 
+int largeValue = Math.Max(fisrtValue,secondValue);
+Console.WriteLine(largeValue); 
