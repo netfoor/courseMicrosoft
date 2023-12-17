3e50c539b8240106d0ef776da725d16f1aabd852
Fortino Romero <2311080254@alumno.utpuebla.edu.mx>
2023-12-17 17:06:23 -0600

    Se realizó un juego de tirada de dados con objeto y instrucciones if

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,13 +1,14 @@
-﻿// Ejercicio de dados
+﻿/*
+// Ejercicio de dados
 
-/*Random dice = new Random(); 
+Random dice = new Random(); 
 int rollOne = dice.Next();
 int rollTwo = dice.Next(101); 
 int rollThree = dice.Next(50, 101);
 
 Console.WriteLine($"First roll: {rollOne}");
 Console.WriteLine($"Second roll: {rollTwo}"); 
-Console.WriteLine($"Thrird roll: {rollThree}");*/
+Console.WriteLine($"Thrird roll: {rollThree}");
 
 //El mayor de dos numeros
 
@@ -16,3 +17,42 @@ int fisrtValue = 500;
 int secondValue = 600; 
 int largeValue = Math.Max(fisrtValue,secondValue);
 Console.WriteLine(largeValue); 
+
+// Método string.Contains
+
+string message = "The quick brown fox jumps over the lazy dog.";
+bool result = message.Contains("Hello"); 
+Console.WriteLine(result); 
+
+if (message.Contains("fox")){
+    Console.WriteLine("What does the fox say?"); 
+}
+
+*/
+// Lanzamiento de 3 dados
+
+Random dice = new Random(); 
+int rollOne = dice.Next(1,7);
+int rollTwo = dice.Next(1,7);  
+int rollThree = dice.Next(1,7); 
+int total = rollOne + rollTwo + rollThree; 
+
+Console.WriteLine($"Dice roll: {rollOne}, {rollTwo}, {rollThree} = {total}"); 
+
+if (rollOne == rollTwo || rollOne == rollThree || rollTwo == rollThree){
+    Console.WriteLine("You rolled doubles!! +2 bonus to total");
+    total += 2;  
+}
+
+if (rollOne == rollTwo && rollTwo == rollThree){
+    Console.WriteLine("You rolled triples!! +6 bonus to total");
+    total += 2;  
+}
+
+if (total >= 15){
+    Console.WriteLine("You win!!");
+}
+if (total < 15){
+    Console.WriteLine("Sorry, you lose."); 
+}
+
