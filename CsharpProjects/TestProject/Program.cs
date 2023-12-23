fb1e6cf952430d6c755c2b0804e3f9ed6ec31ac8
Fortino Romero <2311080254@alumno.utpuebla.edu.mx>
2023-12-22 23:59:45 -0600

    Se completo la primera parate del desafío

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -181,7 +181,6 @@ foreach (char letter in message)
 
 Console.WriteLine(newMessage);
 Console.WriteLine($"The letter 'o' appears {presenceOfTheLetter} times in the text.");
-*/
 
 //Student Grading
 
@@ -291,4 +290,112 @@ foreach (string name in studentNames)
 }
 
 Console.WriteLine("Press the Enter key to continue");
+Console.ReadLine();
+*/
+
+//Reto propuesto
+
+int examAssignments = 5;
+
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
+
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
+
+int[] studentScores = new int[10];
+
+string currentStudentLetterGrade = "";
+
+Console.WriteLine("Student\t\tExam Score\tOverall Grade\tExtra Credit\n");
+
+foreach (string name in studentNames)
+{
+    string currentStudent = name;
+
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
+
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+
+    int sumAssignmentScores = 0;
+
+    decimal currentStudentGrade = 0;
+    decimal extraCredit = 0; 
+
+    int gradedAssignments = 0;
+    int extraAssignments = 0; 
+    int sumAssignmentExtra = 0; 
+    int sumOverallGrade = 0; 
+    
+    foreach (int score in studentScores)
+    {
+        gradedAssignments ++;
+
+        if (gradedAssignments <= examAssignments)
+            sumAssignmentScores += score;
+
+        else
+            sumOverallGrade += score / 10;
+            sumAssignmentExtra += score; 
+            extraAssignments ++;
+
+    }
+    extraCredit = sumAssignmentExtra / extraAssignments; 
+    currentStudentGrade = (decimal)(sumAssignmentScores) / examAssignments;
+    decimal OverallGrade  = (decimal)(sumOverallGrade + sumAssignmentScores) / examAssignments;
+    decimal subtraction = OverallGrade - currentStudentGrade; 
+
+    if (OverallGrade >= 97)
+        currentStudentLetterGrade = "A+";
+
+    else if (OverallGrade >= 93)
+        currentStudentLetterGrade = "A";
+
+    else if (OverallGrade >= 90)
+        currentStudentLetterGrade = "A-";
+
+    else if (OverallGrade >= 87)
+        currentStudentLetterGrade = "B+";
+
+    else if (OverallGrade >= 83)
+        currentStudentLetterGrade = "B";
+
+    else if (OverallGrade >= 80)
+        currentStudentLetterGrade = "B-";
+
+    else if (OverallGrade >= 77)
+        currentStudentLetterGrade = "C+";
+
+    else if (OverallGrade >= 73)
+        currentStudentLetterGrade = "C";
+
+    else if (OverallGrade >= 70)
+        currentStudentLetterGrade = "C-";
+
+    else if (OverallGrade >= 67)
+        currentStudentLetterGrade = "D+";
+
+    else if (OverallGrade >= 63)
+        currentStudentLetterGrade = "D";
+
+    else if (OverallGrade >= 60)
+        currentStudentLetterGrade = "D-";
+
+    else
+        currentStudentLetterGrade = "F";
+
+    
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t\t{OverallGrade}\t{currentStudentLetterGrade}\t{extraCredit} ({subtraction} pts)");
+}
+
+Console.WriteLine("\n\rPress the Enter key to continue");
 Console.ReadLine();
